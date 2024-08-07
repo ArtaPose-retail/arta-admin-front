eeaaef2350129faffb83625c6a63b37a1ce67969
parham <parhamhassanzade.p@gmail.com>
2024-08-07 17:44:47 +0330

    product section ui update

--- src/components/HomePage/ProductsSection.jsx
+++ src/components/HomePage/ProductsSection.jsx
@@ -1,26 +1,17 @@
-import { useState } from "react";
+
 import {
     Box,
-    Divider,
     Grid,
-    InputAdornment,
-    TextField,
 } from "@mui/material";
 import Title from "../UI/Title";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
-import QrCode2Icon from "@mui/icons-material/QrCode2";
 import PtoductSFilter from "./PtoductSFilter";
 import SingleProduct from "./SingleProduct.jsx";
 import { products, refrigeratingProduct } from "../../utils/data.jsx";
-import CloseIcon from "@mui/icons-material/Close";
 import { useSelector } from "react-redux";
-import NewProductParentDialog from "./Dialogs";
+
 function ProductsSection() {
-    const [showQRField, setShowQRField] = useState(false);
     const { showRefrigrateItems } = useSelector((state) => state.product);
-    const showFieldHandler = () => {
-        setShowQRField(!showQRField);
-    };
+
     const center = {
         display: "flex",
         justifyContent: "center",
@@ -45,47 +36,6 @@ function ProductsSection() {
                         color: (theme) => theme.palette.text.card,
                     }}
                 />
-
-                <Box sx={{ ...center, gap: "10px" }}>
-                    <MoreVertIcon fontSize="medium" />
-
-                    {!showQRField ? (
-                        <QrCode2Icon onClick={() => showFieldHandler()} sx={{ cursor: "pointer" }} />
-                    ) : (
-                        <TextField
-                            sx={{
-                                "& .MuiOutlinedInput-notchedOutline": {
-                                    borderColor: "white",
-                                },
-                            }}
-                            type="text"
-                            id="input-with-icon-textfield"
-                            placeholder="کد محصول"
-                            InputProps={{
-                                startAdornment: (
-                                    <InputAdornment
-                                        position="start"
-                                        onClick={() => showFieldHandler()}
-                                        sx={{ cursor: "pointer" }}
-                                    >
-                                        <CloseIcon
-                                            sx={{ fill: (theme) => theme.palette.warning.main }}
-                                        />
-                                    </InputAdornment>
-                                ),
-                                style: {
-                                    background: "#F2F2F2",
-                                    color: "#000",
-                                    direction: "ltr",
-                                    height: "40px",
-                                },
-                            }}
-                            variant="outlined"
-                        />
-                    )}
-                    <Divider orientation="vertical" flexItem />
-                    <NewProductParentDialog type={"add"} />
-                </Box>
             </Box>
             <PtoductSFilter />
             <Grid
--- src/components/HomePage/PtoductSFilter.jsx
+++ src/components/HomePage/PtoductSFilter.jsx
@@ -1,21 +1,9 @@
-import {
-    Box,
-    Divider,
-    FormControlLabel,
-    InputAdornment,
-    Radio,
-    Switch,
-    TextField,
-    Typography,
-} from "@mui/material";
-import { useState } from "react";
-import SearchIcon from "@mui/icons-material/Search";
+import { Box, Divider, Switch, TextField, Typography } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { handleShowRefrigerateitems } from "../../Redux/Slices/HomePage/product";
 import Input from "../UI/Input";
 
 function PtoductSFilter() {
-    const [refrigerat, setRefrigerat] = useState(false);
     const { showRefrigrateItems } = useSelector((state) => state.product);
     const dispatch = useDispatch();
 
@@ -59,68 +47,13 @@ function PtoductSFilter() {
                     </Typography>
                 </option>
             </TextField>
-            <TextField
-                select
-                sx={{
-                    "& .MuiOutlinedInput-notchedOutline": {
-                        borderColor: "white",
-                    },
-                }}
-                type="text"
-                id="input-with-icon-textfield"
-                placeholder="تلفن همراه"
-                InputProps={{
-                    style: {
-                        background: "#F2F2F2",
-                        color: "#000",
-                        direction: "ltr",
-                        height: "40px",
-                    },
-                }}
-                variant="outlined"
-                SelectProps={{
-                    native: true,
-                }}
-            >
-                <option value="">
-                    <Typography
-                        sx={{
-                            fontSize: "12px",
-                            color: (theme) => theme.typography.color,
-                            fontWeight: 400,
-                        }}
-                    >
-                        مقدار خرید
-                    </Typography>
-                </option>
-            </TextField>
+
             <Divider orientation="vertical" flexItem />
-            <Box
-                sx={{
-                    bgcolor: "#F2F2F2",
-                    borderRadius: "7px",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    px: 0.5,
-                }}
-            >
-                <Switch checked={showRefrigrateItems} onClick={(e) => {
-                    handleChange(e)
-                }} />
-
-
-                <Typography sx={{ color: (theme) => theme.palette.divider }}>
-                    سردخانه
-                </Typography>
-            </Box>
-
 
             <Input
                 width={"30%"}
                 icon={"search"}
                 hasIcon={true}
-
                 type={"text"}
                 placeholder={"جستوجو مشتری"}
             />
