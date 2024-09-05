ea096c011dc29390b645a87f1a1fbaae1ba37de6
parham <parhamhassanzade.p@gmail.com>
2024-09-05 11:44:04 +0330

    make no item component

---
+++ src/components/UI/NoItem.jsx
@@ -0,0 +1,15 @@
+import { Box, Typography } from '@mui/material'
+import React from 'react'
+import noItem from "../../Assets/images/NoItem.png"
+import { center } from '../../styles/theme'
+export const NoItem = () => {
+    return (
+        <Box sx={{ ...center, flexDirection: "column", height: "100%", width: "100%" }}>
+            <img src={noItem} alt="no item found" width={400} height={400} />
+            <Typography sx={{ fontSize: "24px", fontWeight: "900", color: theme => theme.palette.primary.main }}>
+                موردی برای نمایش یافت نشد
+            </Typography>
+        </Box>
+    )
+}
+
