712bf96a0641661085dc8cd5730a920c3de667cd
parham <parhamhassanzade.p@gmail.com>
2024-10-27 17:56:15 +0330

    QRCode change

--- src/components/PrintTemplate/PromoCode.jsx
+++ src/components/PrintTemplate/PromoCode.jsx
@@ -1,8 +1,10 @@
-import React, { forwardRef, useEffect } from "react";
+import React, { forwardRef } from "react";
 import { Box, Typography, Grid, Paper, Divider } from "@mui/material";
-import { QRCodeCanvas } from "qrcode.react";
 import { center } from "../../styles/theme";
 import { persianDate, separateBy3, toPersian } from "../../utils/setting";
+import { BakeryDatamatrix } from "@barcode-bakery/barcode-react/datamatrix";
+
+
 const PromoCodePrint = forwardRef((props, ref) => {
     const { data } = props;
 
@@ -11,42 +13,53 @@ const PromoCodePrint = forwardRef((props, ref) => {
     return (
         <Box
             ref={ref}
-            elevation={3}
-            sx={{ padding: 2, width: "80%", borderRadius: "10px" }}
+            sx={{ padding: 2, width: "50%", borderRadius: "10px" }}
         >
             {temp.map((item, index) => (
                 <>
                     <Grid container spacing={2} alignItems="center">
                         {/* QR Code */}
-                        <Grid item xs={4}>
+                        <Grid item xs={5}>
                             <Box sx={{ width: "100%", height: "auto", ...center }}>
-                                <QRCodeCanvas value={`${item?.code}`} />,
+                                <BakeryDatamatrix text={`${item?.code}`} scale={10} />,
                             </Box>
                         </Grid>
 
                         {/* Promo Code Details */}
-                        <Grid item xs={8}>
+                        <Grid item xs={7}>
                             <Box
-                                display="flex"
-                                flexDirection="column"
-                                justifyContent="space-between"
-                                height="100%"
+                                sx={{
+                                    ...center,
+                                    flexDirection: "column",
+                                    justifyContent: "space-between",
+                                    height: "100%",
+                                    width: "100%",
+                                }}
                             >
                                 {/* Promo Code Number */}
                                 <Typography
                                     sx={{
+                                        width: "100%",
                                         border: "1px solid black",
                                         padding: "5px",
                                         borderRadius: "5px",
                                         marginTop: "10px",
+                                        textAlign: "center",
+                                        fontSize: "bold",
                                     }}
-                                    textAlign="center"
-                                    fontWeight="bold"
                                 >
                                     {toPersian(item?.code ?? 0)}
                                 </Typography>
 
-                                <Box sx={{ ...center, width: "100%", my: 1, gap: "5px" }}>
+                                <Box
+                                    sx={{
+                                        ...center,
+                                        height: "100%",
+                                        width: "100%",
+                                        my: 1,
+                                        gap: "5px",
+                                    }}
+                                >
                                     <Typography
                                         textAlign="center"
                                         sx={{
@@ -54,6 +67,7 @@ const PromoCodePrint = forwardRef((props, ref) => {
                                             padding: "5px",
                                             borderRadius: "5px",
                                             width: "50%",
+                                            height: "100%",
                                         }}
                                     >
                                         مدت استفاده تا {persianDate(item?.end_time ?? new Date())}
@@ -67,6 +81,7 @@ const PromoCodePrint = forwardRef((props, ref) => {
                                             padding: "5px",
                                             borderRadius: "5px",
                                             width: "50%",
+                                            height: "100%",
                                         }}
                                     >
                                         {toPersian(item?.amount ?? 0)}{" "}
